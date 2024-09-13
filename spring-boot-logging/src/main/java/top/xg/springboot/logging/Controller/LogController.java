edb2b34c1baf35b4725f3c7d5857803a51e921e4
yian2909 <915826506@qq.com>
2024-09-13 13:58:41 +0800

    Add files via upload

---
+++ spring-boot-logging/src/main/java/top/xg/springboot/logging/Controller/LogController.java
@@ -0,0 +1,22 @@
+package top.xg.springboot.logging.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+    private static final Logger logger= LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/log")
+    public String logDemo(){
+        logger.trace("This is a TRACE log");
+        logger.debug("This is a DEBUG log");
+        logger.info("This is an INFO log");
+        logger.warn("This is a WARN log");
+        logger.error("This is an ERROR log");
+        return"Logs have been generated!";
+    }
+
+}
