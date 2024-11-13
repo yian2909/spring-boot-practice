6c8f7cff436424ea7efbdcd4f7954ea27ad1ce95
yian <915826506@qq.com>
2024-11-13 23:09:57 +0800

    整合jwt、token

--- base-web/src/main/java/top/xg/web/sys_user/controller/SysUserController.java
+++ base-web/src/main/java/top/xg/web/sys_user/controller/SysUserController.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.web.bind.annotation.*;
+import top.xg.jwt.JwtUtils;
 import top.xg.result.ResultVo;
 import top.xg.utils.ResultUtils;
 import top.xg.web.sys_menu.entity.AssignTreeParm;
@@ -29,9 +30,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * @author Lenovo
@@ -43,6 +42,7 @@ public class SysUserController {
     private final SysUserService sysUserService;
     private final SysUserRoleService sysUserRoleService;
     private final DefaultKaptcha defaultKaptcha;
+    private final JwtUtils jwtUtils;
 
     @GetMapping("/getRoleList")
     @Operation(summary = "根据用户id查询用户的角色")
@@ -167,6 +167,11 @@ public class SysUserController {
         LoginVo vo = new LoginVo();
         vo.setUserId(one.getUserId());
         vo.setNickName(one.getNickName());
+        //生成token
+        Map<String,String> map= new HashMap<>();
+        map.put("userId",Long.toString(one.getUserId()));
+        String token=jwtUtils.generateToken(map);
+        vo.setToken(token);
         return ResultUtils.success("登录成功",vo);
     }
 
