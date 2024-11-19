d8deeaf1cfdb0c51c3dd88feebdf1ff58bfac085
yian <915826506@qq.com>
2024-11-19 10:10:38 +0800

    日志管理模块

---
+++ spring-boot-logging/src/main/java/top/xg/springboot/logging/Application.java
@@ -0,0 +1,17 @@
+package top.xg.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * @author gx
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
